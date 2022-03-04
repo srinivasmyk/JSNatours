66e60beb7f26b1dc07c5975331dca9b5708d2c43
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2022-03-04 19:47:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,6 @@
  //  var config={kcAllowedFuncNames:["zendeskchat"]};
    
  
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-KNFBP3CQEW3C-2");
 
 
 function validate() {
